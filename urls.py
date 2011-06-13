9b22e4a38ccd3497905f03567b8690e799a2bac9
Emmanuel Okyere <chief@hutspace.net>
2011-06-13 21:42:09 +0000

    Fix errors with admin urls

--- urls.py
+++ urls.py
@@ -12,6 +12,6 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-     url(r'^$', include(admin.site.urls)),
-     url(r'^admin/$', include(admin.site.urls)),
+     url(r'^admin/', include(admin.site.urls)),
+#     url(r'^$', include(admin.site.urls)),
 )
